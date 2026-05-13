0f92350535316d778014eaa7692221365c52059b
Vikesh K <kvikesh10052005@gmail.com>
2026-05-13 12:12:37 +0530

    Modified

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-let ele = document.getElementById('child');
+let ele = document.querySelector('#heading');
 console.log(ele);
+
+console.log(ele.textContent);
+
+ele.textContent = "<P>Hello World</P>";
+
+ele.innerHTML = "<p>Chennai</p>";
+console.log(ele.innerHTML);
+
+console.log(ele.id);
+ele.id = "domHeading";
+console.log(ele.id);
+
+console.log(ele.childNodes);
